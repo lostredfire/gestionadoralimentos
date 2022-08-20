6cd3d8c6533eb948a5daefad46baa0731fcf6663
lostredfire <63705225+lostredfire@users.noreply.github.com>
2022-08-20 15:17:32 +0200

    Create UserInterfaceI.java

---
+++ basegestionadoralimentos/src/vista/UserInterfaceI.java
@@ -0,0 +1,10 @@
+package vista;
+
+public interface UserInterfaceI {
+	
+	public void mostrarMenu();
+	public void mostrarOK();
+	public void mostrarError();
+	public void mostrarListaAlimentos();
+	
+}
